ab0fdc5d87b7b8f6e238736bfcd63fff8b9ea856
unknown <bensandlin2@gmail.com>
2023-01-13 16:53:35 -0600

    Completed exercise 13

--- main.py
+++ main.py
@@ -163,3 +163,29 @@ while number > 0:
     digit = number % 10
     number = number // 10
     print(digit, end=" ")
+
+#exercise 12
+#calculate incoming tax for the given income by adhering to the below rules~
+#(a table is displayed)
+income = 45000
+tax_payable = 0
+print("Given income", income)
+
+if income <= 10000:
+    tax_payable = 0
+elif income <= 20000:
+    # no tax on first 10,000
+    x = income - 10000
+    # 10% tax
+    tax_payable = x * 10 / 100
+else:
+    # first 10,000
+    tax_payable = 0
+
+    # next 10,000 10% tax
+    tax_payable = 10000 * 10 / 100
+
+    # remaining 20%tax
+    tax_payable += (income - 20000) * 20 / 100
+
+print("Total tax to pay is", tax_payable)
