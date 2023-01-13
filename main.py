4d6bf6064f240509ef8957db4d79accf4036b159
unknown <bensandlin2@gmail.com>
2023-01-13 17:11:20 -0600

    Completed exercise 15

--- main.py
+++ main.py
@@ -205,3 +205,15 @@ for i in range(6, 0, -1):
     for j in range(0, i - 1):
         print("*", end=' ')
     print(" ")
+
+#Exercise 15
+#Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+def exponent(base, exp):
+    num = exp
+    result = 1
+    while num > 0:
+        result = result * base
+        num = num - 1
+    print(base, "raises to the power of", exp, "is: ", result)
+exponent(5, 4)
